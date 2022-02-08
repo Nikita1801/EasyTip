467b63c75fd9a8dfd9204084d17603da0033a7b7
nikitamakarevich <nikita-makarevich@mail.ru>
2022-02-08 16:08:48 +0300

    Initial Commit

---
+++ Tipsy/ViewController.swift
@@ -0,0 +1,28 @@
+
+
+import UIKit
+
+class CalculatorViewController: UIViewController {
+
+    @IBOutlet weak var billTextField: UITextField!
+    
+    @IBOutlet weak var zeroPtcBitton: UIButton!
+    @IBOutlet weak var tenPtcButton: UIButton!
+    @IBOutlet weak var twentyPtcButton: UIButton!
+    
+    @IBOutlet weak var splitNumberLable: UILabel!
+    
+
+    
+    @IBAction func tipChanged(_ sender: UIButton) {
+    }
+    
+    @IBAction func stepperValueChanged(_ sender: UIStepper) {
+    }
+    
+    @IBAction func calculatePressed(_ sender: UIButton) {
+    }
+    
+
+}
+
