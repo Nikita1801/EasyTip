a934d9679c94d438057917156b9d47e8b667dfd8
nikitamakarevich <nikita-makarevich@mail.ru>
2022-02-08 20:52:26 +0300

    Solved decimal issue (there was no way to type "." instead of "," So app crush every time when user type "," because of double type)

--- Tipsy/Controllers/ResultViewController.swift
+++ Tipsy/Controllers/ResultViewController.swift
@@ -4,6 +4,11 @@ import Foundation
 import UIKit
 
 class ResultViewController : UIViewController{
+    
+    var result = "0.0"
+    var percent = 10
+    var people = 2
+    
     @IBOutlet weak var totalLable: UILabel!
     
     @IBOutlet weak var peopleSettingsLable: UILabel!
@@ -11,8 +16,15 @@ class ResultViewController : UIViewController{
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        totalLable.text = result
+        peopleSettingsLable.text = "Split between \(people) people"
+        percentSettingsLable.text = "Tip is \(percent)%"
     }
     
     @IBAction func recalculatePressed(_ sender: UIButton) {
+        self.dismiss(animated: true, completion: nil)
     }
+    
+    
 }
--- Tipsy/Controllers/ViewController.swift
+++ Tipsy/Controllers/ViewController.swift
@@ -12,9 +12,10 @@ class CalculatorViewController: UIViewController {
     
     @IBOutlet weak var splitNumberLable: UILabel!
     
-    var bill = "1"
+    var billTotal = 0.0
     var percent = 0.1
     var people = 2
+    var finalResult = "0.0"
     
     
     @IBAction func tipChanged(_ sender: UIButton) {
@@ -30,25 +31,42 @@ class CalculatorViewController: UIViewController {
             percent = 0.2
         }
         
+        billTextField.endEditing(true)
+        
     }
     
     @IBAction func stepperValueChanged(_ sender: UIStepper) {
         people = Int(sender.value)
         splitNumberLable.text = String(people)
+        
     }
     
     @IBAction func calculatePressed(_ sender: UIButton) {
-        print(percent)
-        print(people)
-        bill = billTextField.text!
-        print(bill)
-        billTextField.endEditing(true)
+        let bill = String(billTextField.text!.replacingOccurrences(of: ",", with: "."))
+        if (bill != "") {
+            billTotal = Double(bill)!
+            let result = ((1 + percent) * billTotal) / Double(people)
+            let resultTo2DecimalPlaces = String(format: "%.2f", result)
+            finalResult = resultTo2DecimalPlaces
+            print(finalResult)
+            
+            self.performSegue(withIdentifier: "ResultView", sender: self)
+        }
     }
     
     func updateUI() {
         zeroPtcBitton.isSelected = false
         tenPtcButton.isSelected = false
         twentyPtcButton.isSelected = false
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if (segue.identifier == "ResultView") {
+            let controller = segue.destination as! ResultViewController
+            controller.result = finalResult
+            controller.percent = Int(percent * 100)
+            controller.people = people
+        }
     }
 
 }
